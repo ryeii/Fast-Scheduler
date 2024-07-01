633692e26707c19eb6fe8a5ecd49628486c5e7e7
ryeii <anzhiyu@berkeley.edu>
2024-07-01 02:05:14 -0700

    r

---
+++ main.py
@@ -0,0 +1,82 @@
+import gradio as gr
+from scheduler import *
+
+def update_input_data(input_file):
+    if input_file is None:
+        return None, None, pd.DataFrame(), ""
+    
+    # Save the uploaded file
+    input_path = "input_data.xlsx"
+    with open(input_path, "wb") as file:
+        file.write(open(input_file.name, "rb").read())
+
+    # Create a Scheduler instance with the input data
+    scheduler = Scheduler(input_path)
+
+    # Get the input data as DataFrames
+    timeslots_df, student_preferences_df = scheduler.get_data()
+
+    return timeslots_df, student_preferences_df, pd.DataFrame(), ""
+
+def schedule_courses(input_file, timeslots_df, student_preferences_df):
+    # Save the input data to a file
+    input_path = "input_data.xlsx"
+    with pd.ExcelWriter(input_path) as writer:
+        timeslots_df.to_excel(writer, sheet_name="timeslots", index=False)
+        student_preferences_df.to_excel(writer, sheet_name="student preferences", index=False)
+
+    # Create a Scheduler instance with the input data
+    scheduler = Scheduler(input_path)
+
+    # Run the scheduling process
+    success = scheduler.schedule()
+
+    # If the scheduling is successful, read the result file and log file
+    if success:
+        result_df = pd.read_excel("result.xlsx")
+        with open("log.txt", "r") as file:
+            log_text = file.read()
+        return result_df, log_text
+    else:
+        error_df = pd.DataFrame({"Error": ["An error occurred during scheduling."]})
+        return error_df, "An error occurred during scheduling."
+
+# Create a Gradio interface using Blocks
+with gr.Blocks() as iface:
+    gr.Markdown("# Fast Course Scheduler")
+    
+    with gr.Row():
+        with gr.Column():
+            gr.Markdown("""In this column:\n
+                        1. upload the timeslots and student preferences file then click "submit" to update the input data.\n
+                        1. click "schedule" to run the scheduling process.\n
+                        1. modify the input data in the table and schedule again.""")
+        with gr.Column():
+            gr.Markdown("""In this column:\n
+                        1. view the log to see a summary of the scheduling results.\n
+                        1. view the scheduled courses for each student.""")
+    
+    with gr.Row():
+        with gr.Column():
+            input_file = gr.File(label="Timeslots & Student Preferences File")
+            submit_button = gr.Button("Submit")
+            timeslots_output = gr.Dataframe(label="Timeslots")
+            schedule_button = gr.Button("Schedule")
+            student_preferences_output = gr.Dataframe(label="Student Preferences")
+        with gr.Column():
+            log_output = gr.Textbox(label="Log")
+            scheduled_courses_output = gr.Dataframe(label="Scheduling Results")
+    
+    submit_button.click(
+        fn=update_input_data,
+        inputs=input_file,
+        outputs=[timeslots_output, student_preferences_output, scheduled_courses_output, log_output],
+    )
+    
+    schedule_button.click(
+        fn=schedule_courses,
+        inputs=[input_file, timeslots_output, student_preferences_output],
+        outputs=[scheduled_courses_output, log_output],
+    )
+
+iface.launch()
---
+++ scheduler.py
@@ -0,0 +1,125 @@
+import pandas as pd
+from collections import defaultdict
+from itertools import product, combinations
+import os
+
+class Scheduler:
+    def __init__(self, data_path):
+        self.path = data_path
+
+    def get_data(self):
+        df = pd.read_excel(self.path, sheet_name=['timeslots', 'student preferences'])
+        return df.get('timeslots'), df.get('student preferences')
+    
+    def preprocess(self):
+        '''
+        This preprocess is implemented specifically for Beijing Aidi School and may not be applicable to other schools.
+        '''
+        timeslots, student_preferences = self.get_data()
+
+        print('Preprocessing...')
+
+        # create new dataframe student_preferences_processed
+        student_preferences_processed = pd.DataFrame(columns=['Student name', 'Fixed class 1', 'Fixed class 2', 
+                                                              'Fixed class 3', 'Fixed class 4', 'Elective 1', 
+                                                              'Elective 2', 'Elective 3', ])
+
+        # for each row of the student preferences, if the number of classes is larger than the number of timeslots
+        for _, row in student_preferences.iterrows():
+            electives = row[5:].dropna()
+            fixed = row[1:5].dropna()
+            name = row[0]
+            # for each combinations of three classes in electives, create a new row with the same fixed classes and the combination of three classes. the combinations are without repetition.
+            for combination in combinations(electives, 3):
+                    student_preferences_processed.loc[len(student_preferences_processed)] = [name, *fixed, *combination]
+            if len(electives) < 3:
+                student_preferences_processed.loc[len(student_preferences_processed)] = [name, *fixed, *electives, *[None for _ in range(3-len(electives))]]
+
+        print('Number of students after preprocessing: ', len(set(student_preferences_processed['Student name'])))
+        
+        return timeslots, student_preferences_processed
+                
+
+    def schedule(self, save_path='result.xlsx'):
+
+        try:
+            print('Scheduling...')
+            timeslots, student_preferences = self.preprocess()
+
+            # Create a dictionary to map classes to their available slots
+            class_to_slots = {}
+            for col_number, classes in enumerate(timeslots.columns, start=1):
+                for class_name in timeslots[classes].dropna():
+                    if class_name not in class_to_slots:
+                        class_to_slots[class_name] = []
+                    class_to_slots[class_name].append(col_number)
+
+            # Compute the valid schedules for each student
+            valid_schedules = {}
+            list_failed = []
+            for _, row in student_preferences.iterrows():
+                student = row[0]
+                classes = row[1:]
+
+                # if len of classes is smaller than len col of timeslots, add the student to list_failed and skip to next student
+                if (len(classes) < len(timeslots.columns)) or (not all([class_name in class_to_slots for class_name in classes])):
+                    list_failed.append(student)
+                    continue
+
+                if student not in valid_schedules:
+                    valid_schedules[student] = []
+
+                # Compute the Cartesian product of the available slots for the chosen classes
+                class_combinations = list(product(*[class_to_slots[class_name] for class_name in classes]))
+
+                # Filter out combinations where not all slots are unique
+                unique_combinations = [combination for combination in class_combinations if len(set(combination)) == len(combination)]
+                if unique_combinations:
+                    for combination in unique_combinations:
+                        valid_schedules[student].append([f'{class_name} (slot {slot})' for class_name, slot in zip(classes, combination)])
+                if (not valid_schedules[student]) and (student not in list_failed):
+                    list_failed.append(student)
+                
+                # if there is an entry in valid_schedules for the student, take down all names of this student in list_failed
+                if valid_schedules[student] and (student in list_failed):
+                    while student in list_failed:
+                        list_failed.remove(student)
+
+            # Create a dataframe to save the results
+            df = pd.DataFrame(columns=['Student name', *[f'course {i+1}' for i in range(len(timeslots.columns))]])
+            for student, schedules in valid_schedules.items():
+                for schedule in schedules:
+                    df = df._append({'Student name': student, **{f'course {i+1}': course for i, course in enumerate(schedule)}}, ignore_index=True)
+            for student in list_failed:
+                df = df._append({'Student name': student, **{f'course {i+1}': 'No valid schedule' for i in range(len(timeslots.columns))}}, ignore_index=True)
+            
+            # Save the results to an excel file
+            df.to_excel(save_path, index=False)
+        
+        except Exception as e:
+            print(e)
+            with open('log.txt', 'w') as f:
+                f.write('\n')
+                f.write('no log yet\n')
+            return False
+
+        # generate log. number of students, number of students with no solution, percentage of students with at least one solution. save to log.txt
+        with open('log.txt', 'w') as f:
+            f.write('\n')
+            f.write(f'~ Schedule created for {self.path}\n')
+            f.write(f'###   Schedule Log   #############################################################\n')
+            # names = names of all students
+            success_names = set(student_preferences['Student name'])
+            f.write(f'~ Number of students: {len(success_names)}\n')
+            f.write(f'~ Number of students with valid schedule(s): {len(success_names) - len(set(list_failed))}\n')
+            f.write(f'~ Percentage of students with valid schedule(s): {100 - len(set(list_failed))/len(success_names)*100}%\n')
+            f.write('Result saved.\n')
+            # f.write(f'###   Info Dump   ################################################################\n')
+            # # write df
+            # f.write(f'~ Dataframe: \n{df}\n')
+            f.write(f'###   Debug Info   ###############################################################\n')
+            f.write(f'~ Timeslot No. = {len(timeslots.columns)}\n')
+            f.write(f'~ Number of classes = {len(class_to_slots)}\n')
+            f.write(f'~ List of classes = {list(class_to_slots.keys())}\n')
+        
+        return True
